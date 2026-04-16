698f47f893fc69ee55838a99420a0fe141a7f59e
luigimnegri <luigi1997negri@gmail.com>
2026-04-16 10:29:34 +0200

    fix

---
+++ FriendMapSeed/FriendMapSeed/FriendMapSeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendMapSeedApp.swift
+//  FriendMapSeed
+//
+//  Created by luigi negri on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct FriendMapSeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
